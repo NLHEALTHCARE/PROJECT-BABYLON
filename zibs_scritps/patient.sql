9f06f27c8c69538c0c461a24a952d5576ca47ecc
TxEddy <eddy@MBA-Eddy.local>
2017-04-03 11:33:27 +0200

    Zorginformatiebouwsteen Patient

--- zibs_scritps/patient.sql
+++ zibs_scritps/patient.sql
@@ -1,12 +1,31 @@
 select
-   ifct_bsn                    as "identifier"
-   , null                      as "active"
-   , null                      as "name_use" -- ZIBS Patient definieert Naamgebruikcodes https://zibs.nl/wiki/Patient(NL)#NaamgebruikCodelijst
-   , null                      as "name_text"
-   , ifct_achternaam           as "name_familiy"
-   , ifct_voornaam             as "name_given"
-   , ifct_geslacht             as "gender"
-   , v_intf_contact.ifct_geboortedtm as "birthdate"
-
+   ifct_bsn                                     as "identifier"                 -- PatientIdentificatienummer
+   , null                                       as "active"                     -- Is patientenrecord in gebruik
+   , null                                       as "name_use"                   -- zib's Patient definieert Naamgebruikcodes https://zibs.nl/wiki/Patient(NL)#NaamgebruikCodelijst
+   , null                                       as "name_text"                  -- Roepnaam
+   , ifct_achternaam                            as "name_familiy"               -- Geslachtsnaam_Achternaam
+   , ifct_voornaam                              as "name_given"                 -- Voornaam
+   , null                                       as "telecom_system"             -- zib's Patient definieert NummerSoortCodes: https://zibs.nl/wiki/Patient(NL)#NummerSoortCodelijst
+   , null                                       as "telecom_value"              -- zib's Patient definieert een Telefoonnummer.
+   , ifct_geslacht                              as "gender"                     -- Geslacht
+   , v_intf_contact.ifct_geboortedtm            as "birthdate"                  -- Geboortedatum
+   , v_intf_contact.ifct_vervaldtm              as "deceased"                   -- OverlijdingsIndicator
+   , v_intf_contact.ifct_straat_b               as "address_text"               -- AdresGegevens_straat
+   , v_intf_contact.ifct_huisnr_b               as "address_text"               -- AdresGegevens_huisnummer
+   , null                                       as "address_line"               -- zib's Patient definieert bij AdresGegevens een Huisnummerletter.
+   , v_intf_contact.ifct_gebouw_b               as "address_line"               -- zib's Patient definieert bij AdresGegevens een Huisnummertoevoeging.
+   , null                                       as "address_line"               -- zib's Patient definieert AanduidingBijNummerCodes: https://zibs.nl/wiki/Patient(NL)#AanduidingBijNummerCodelijst
+   , v_intf_contact.ifct_postcode_b             as "address_postalCode"         -- AdresGegevens_postcode
+   , v_intf_contact.ifct_plaats_b               as "adress_city"                -- AdresGegevens_woonplaats
+   , null                                       as "address"                    -- zib's Patient definieert bij AdresGegevens een Gemeente.
+   , v_intf_contact.ifct_lnd_omschrijving_b     as "address_country"            -- zib's Patient definieert bij AdresGegevens twee verschillende LandCodes: https://zibs.nl/wiki/Patient(NL)#LandGBACodelijst
+   , null                                       as "address_line"               -- zib's Patient definieert bij AdresGegevens AdditioneleInformatie.
+   , v_intf_contact.ifct_is_meerling            as "multiple_birth"             -- MeerlingIndicator.
+   , v_intf_contact.ifct_voorletters_partner    as "contact_name_prefix"        -- VoorvoegselsPartner.
+   , v_intf_contact.ifct_achternaam_partner     as "contact_name_family"        -- AchternaamPartner.
+   , null                                       as "contact_"                   -- PartnerPatient_Identificatienummer.
+   , v_intf_contact.ifct_geslacht_partner       as "contact_gender"             -- PartnerGeslacht.
+   , null                                       as "contact_"                   -- zib's definieert bij Patient_Partner MeerlingIndicator, FHIR niet en staat ook niet in database.
+   , null                                       as "contact_period"             -- zib's definieert bij Patient_Partner Overlijdindicator, staat niet in databse.
 from mtdx.v_intf_contact 
-where ifct_soort = 9            -- dit zijn patienten
+where ifct_soort = 9                                                            -- Dit zijn patienten
